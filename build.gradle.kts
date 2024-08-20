33b2103dae7924c8b3e67f4a16ac1c8830d97302
Laxman Ch <laxman@traceable.ai>
2024-08-20 16:24:15 +0530

    upgrade to latest gradle and gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("base")
-  id("org.hypertrace.docker-plugin") version "0.9.9"
-  id("org.hypertrace.docker-publish-plugin") version "0.9.9"
+  id("org.hypertrace.docker-plugin") version "0.10.1"
+  id("org.hypertrace.docker-publish-plugin") version "0.10.1"
 }
 
 hypertraceDocker {
