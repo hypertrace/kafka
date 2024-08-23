1b59136e255e7c2cda7c420f5b5f7af93444902c
Ravi Singal <ravi.singal@traceable.ai>
2024-08-23 12:51:51 +0530

    setup new module for kafka docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
 plugins {
   id("base")
-  id("org.hypertrace.docker-plugin") version "0.10.1"
-  id("org.hypertrace.docker-publish-plugin") version "0.10.1"
-}
-
-hypertraceDocker {
-  defaultImage {
-    imageName.set("kafka")
-  }
+  id("org.hypertrace.docker-publish-plugin") version "0.10.1" apply false
 }
